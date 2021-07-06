b3bb624493907a9187805611b4718a16a6bac5d0
ChrisAndrade <chriskevinandrade@gmail.com>
2021-07-05 23:45:04 -0400

    Made a basic home page
    link to giphy has been added. Other pages will be added soon

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+import ReactDOM from 'react-dom';
+
+class Homepage extends Component{
+    render(){
+        return(
+            
+            <Link to= "/giphy" className= "makeResButton"> GIPHY Page</Link>
+            
+        )
+    }
+}export default Homepage;
